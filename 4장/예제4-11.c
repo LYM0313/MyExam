60b94176b38921f50e1cb7240447b6c326eadb0e
LYM0313 <ymlee010313@gmail.com>
2025-07-29 14:07:56 +0900

    Create 예제4-11.c

---
+++ 4장/예제4-11.c
@@ -0,0 +1,15 @@
+#define _CRS_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main() {
+	int a = 10, b = 20;
+	int res;
+
+	res = (++a, ++b);
+
+	printf("a:%d, b:%d\n", a, b);
+	printf("res:%d\n", res);
+
+	return 0;
+}
