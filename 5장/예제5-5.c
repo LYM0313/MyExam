630f2a481e53b7841561bfe3efcb759159c7158f
LYM0313 <ymlee010313@gmail.com>
2025-07-29 15:06:18 +0900

    Create 예제5-5.c

---
+++ 5장/예제5-5.c
@@ -0,0 +1,17 @@
+#define _CRS_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+int main() {
+	int a = 10, b = 20;
+
+	if (a < 0) {
+		if (b > 0) {
+			printf("ok");
+		}
+	}
+	else {
+		printf("ok");
+	}
+	return 0;
+}
