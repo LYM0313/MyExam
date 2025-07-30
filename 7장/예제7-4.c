b706def87d060797bde88f9efe92532363162f28
LYM0313 <ymlee010313@gmail.com>
2025-07-30 09:40:43 +0900

    Create 예제7-4.c

---
+++ 7장/예제7-4.c
@@ -0,0 +1,20 @@
+#define CRS_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+void print_char(char ch, int count);
+
+int main() {
+	print_char('@', 5);
+
+	return 0;
+}
+
+void print_char(char ch, int count) {
+	int i;
+
+	for (i = 0; i < count; i++) {
+		printf("%c", ch);
+	}
+	return;
+}
