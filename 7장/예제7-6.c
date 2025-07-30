90386ed584268d45d8e6e94db0b3f72d6ead3323
LYM0313 <ymlee010313@gmail.com>
2025-07-30 09:47:49 +0900

    Create 예제7-6.c

---
+++ 7장/예제7-6.c
@@ -0,0 +1,20 @@
+#define CRS_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+void fruit(int count);
+
+int main() {
+	fruit(1);
+
+	return 0;
+}
+
+void fruit(int count) {
+	printf("apple\n");
+	if (count == 3) {
+		return;
+	}
+	fruit(count + 1);
+}
+
